4704c3a25646d415814d5b2f9f714c473cd2db6a
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2023-07-24 22:36:18 +0700

    finished css

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
     plugins: ['prettier-plugin-tailwindcss'],
 
+    "printWidth": 50
+
 }
 
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import {
+  Form,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 
@@ -35,7 +40,8 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting =
+    navigation.state === "submitting";
 
   const formErrors = useActionData();
 
@@ -43,31 +49,58 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let's go!
+      </h2>
       {/* <Form method="POST" action="/order/new"></Form> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+          />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">
+            Phone number
+          </label>
+          <div className="grow">
+            <input
+              type="tel"
+              name="phone"
+              required
+              className="input  w-full"
+            />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">
+            Address
+          </label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -76,13 +109,27 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label
+            htmlFor="priority"
+            className="font-medium"
+          >
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
-          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order..." : "Order now"}
+          <input
+            type="hidden"
+            name="cart"
+            value={JSON.stringify(cart)}
+          />
+          <Button
+            disabled={isSubmitting}
+            type="primary"
+          >
+            {isSubmitting
+              ? "Placing order..."
+              : "Order now"}
           </Button>
         </div>
       </Form>
@@ -106,7 +153,8 @@ export async function action({ request }) {
   if (!isValidPhone(order.phone))
     errors.phone =
       "Please give us your correct phone number. We might need it to contact you";
-  if (Object.keys(errors).length > 0) return errors;
+  if (Object.keys(errors).length > 0)
+    return errors;
 
   //If everything is okey, create new order and redirect
   const newOrder = await createOrder(order);
